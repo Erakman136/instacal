dd96cb8eb6ed5d417cec16882c97da723b36d854
Erakman136 <165496559+Erakman136@users.noreply.github.com>
2026-02-04 17:57:54 +0300

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -33,7 +33,7 @@ app.post('/api/login', (req, res) => {
 
 // React build klasörünü serve etmek için (frontend deploy’u tek URL’de çalıştırmak istersen)
 app.use(express.static(path.join(__dirname, 'frontend/dist')));
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
 });
 
